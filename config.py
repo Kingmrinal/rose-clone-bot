da352911673faf2d6a085c3b454c87caac028c27
Kingmrinal <163413124+Kingmrinal@users.noreply.github.com>
2025-10-04 18:44:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")  # Telegram Bot Token
+
+WELCOME_MESSAGE = "Welcome to the group, {first_name} 👋"
+
+BAD_WORDS = ["badword1", "badword2"]
+
+ANTI_LINK = True
+
+FLOOD_INTERVAL = 2
